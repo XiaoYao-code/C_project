341d4d362cf5c7e64591f11641d6ea3fae255667
姚鑫原 <1774164628@qq.com>
2020-04-04 21:35:37 +0800

    测试

---
+++ test_4_4/test_4_4/practise.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	//喝汽水 20元 1元一瓶  2个瓶子换一瓶，可以喝几瓶汽水
+//	int count = 0;
+//	int s = 20;
+//	int y = 0;
+//	count = s;
+//	while (s>1)
+//	{
+//		y = s % 2;
+//		s = s / 2;
+//		count = s + count;
+//		s = s + y;
+//	
+//	}
+//	printf("你可以喝%d瓶汽水.\n", count);
+//	return 0;
+//}
