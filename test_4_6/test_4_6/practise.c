658221d3f560a3af2e84e095e8dd46d859960644
姚鑫原 <1774164628@qq.com>
2020-04-06 23:05:31 +0800

    改进

---
+++ test_4_6/test_4_6/practise.c
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int g_val = 100;
+//int test(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int arr[20] = { 0 };
+//	int i = 0;
+//	int sum = test(2, 3);
+//	g_val = 200;
+//	for (i = 0; i < 10; i++)
+//	{
+//	arr[i] = i;
+//	}
+//
+//	for (i = 10; i < 20; i++)
+//	{
+//	arr[i] = i;//¶Ïµã
+//	}
+//	return 0;
+//}
+//
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//void test1()
+//{
+//	test2();
+//}
+//void test()
+//{
+//	test1();
+//}
+//int main()
+//{
+//	test();
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);//3
+//	//1!+2!+3!
+//	//1+2+6 = 9
+//	int i = 0;
+//	int ret = 1;
+//	int j = 0;
+//	int sum = 0;
+//
+//	for (j = 1; j <= n; j++)
+//	{
+//	//¼ÆËãjµÄ½×³Ë
+//	ret = 1;
+//	for (i = 1; i <= j; i++)
+//	{
+//	ret *= i;
+//	}
+//	sum += ret;
+//	}
+//
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);//3
+//	//1!+2!+3!
+//	//1+2+6 = 9
+//	int i = 0;
+//	int ret = 1;
+//	int j = 0;
+//	int sum = 0;
+//
+//	for (j = 1; j <= n; j++)
+//	{
+//	ret *= j;
+//	sum += ret;
+//	}
+//
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+//
+
+//
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	for (i = 0; i <=12; i++)
+	{
+	arr[i] = 0;
+	printf("hehe\n");
+	}
+
+	return 0;
+}
